8ba1582cf2aaec57378ffe86109de0cab424230f
amirafathy-tech <amira.fathy@solex.tech>
2025-02-04 10:52:32 -0800

    Main-AppRouter

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  // apiUrl: 'https://sd-dev.c-878bd9e.kyma.ondemand.com',
+  apiUrl: 'https://sd-cf.cfapps.us10-001.hana.ondemand.com',
+  authUrl:"https://ajwgvqm4q.trial-accounts.ondemand.com/oauth2/token",
+  clientID: "1830c61b-9601-4c91-a0c5-901cdbf0c2e4",
+  clientSecret: "mGV7xPbl[MV51HG-lVeHQV2=LkTg=@"
+};
