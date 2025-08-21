d3d869946479b74a8eb20d1ba37de7bed071c01f
amirafathy-tech <amira.fathy@solex.tech>
2025-08-21 16:59:10 -0700

    add import from excel button for the service master

--- src/app/new-service-master/add-edit-service-master/add-edit-service-master.component.ts
+++ src/app/new-service-master/add-edit-service-master/add-edit-service-master.component.ts
@@ -6,6 +6,8 @@ import { ApiService } from 'src/app/shared/ApiService.service';
 import { ServiceMaster } from '../../new-service-master/new-service-master.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceMasterService } from '../new-service-master.service';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-add-edit-service-master',
@@ -14,6 +16,134 @@ import { ServiceMasterService } from '../new-service-master.service';
   providers: [ApiService, ServiceMasterService, MessageService, ConfirmationService]
 })
 export class AddEditServiceMasterComponent implements OnInit {
+
+
+  displayImportsDialog = false;
+  displayExcelDialog = false;
+  // Excel Import:
+  parsedData: ServiceMaster[] = []; // Parsed data from the Excel file
+  displayedColumns: string[] = []; // Column headers from the Excel file
+
+  // imports:
+  showImportsDialog() {
+    this.displayImportsDialog = true;
+  }
+  showExcelDialog() {
+    this.displayExcelDialog = true;
+  }
+
+  onFileSelect(event: any, fileUploader: any) {
+    console.log('Records before :', this.parsedData);
+    const file = event.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const binaryData = e.target.result;
+      const workbook = XLSX.read(binaryData, { type: 'binary' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const jsonData: any[][] = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      if (jsonData.length > 0) {
+        this.displayedColumns = jsonData[0].filter((col: any) => typeof col === 'string' && col.trim() !== '') as string[];
+        this.parsedData = jsonData.slice(1).map((row: any[]) => {
+          const rowData: any = {};
+          this.displayedColumns.forEach((col, index) => {
+            rowData[col] = row[index] !== undefined ? row[index] : '';
+          });
+          return rowData;
+        });
+        console.log('Records :', this.parsedData);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'Records copied from the excel sheet successfully!',
+          life: 4000
+        });
+
+        
+      // After showing success → ask for confirmation
+      setTimeout(() => {
+        this.confirmationService.confirm({
+          message: 'Do you want to save the copied records?',
+          header: 'Confirm Save',
+          icon: 'pi pi-question-circle',
+          accept: () => {
+            // Loop through parsedData and call service
+            this.parsedData.forEach(record => {
+              this.saveServiceMasterFromExcel(record)
+              // .subscribe({
+              //   next: () => console.log('Saved:', record),
+              //   error: (err) => console.error('Error saving:', record, err)
+              // });
+            });
+            this.messageService.add({
+              severity: 'info',
+              summary: 'Saved',
+              detail: 'All records have been saved.'
+            });
+          },
+          reject: () => {
+            this.messageService.add({
+              severity: 'warn',
+              summary: 'Cancelled',
+              detail: 'Records were not saved.'
+            });
+          }
+        });
+      }, 500);
+
+      } else {
+        this.displayedColumns = [];
+        this.parsedData = [];
+      }
+      // Reset the file input using the PrimeNG method
+      fileUploader.clear();
+    };
+    reader.readAsBinaryString(file);
+  }
+
+  // for selected from excel sheet:
+  saveServiceMasterFromExcel(serviceMaster: ServiceMaster) {
+    console.log(serviceMaster);
+     const newRecord = new ServiceMaster(serviceMaster.searchTerm, serviceMaster.description,
+        serviceMaster.serviceText, serviceMaster.shortTextChangeAllowed,
+        serviceMaster.deletionIndicator, serviceMaster.mainItem, serviceMaster.numberToBeConverted,
+        serviceMaster.convertedNumber,
+        serviceMaster.serviceTypeCode, serviceMaster.materialGroupCode,
+        serviceMaster.baseUnitOfMeasurement, serviceMaster.toBeConvertedUnitOfMeasurement, serviceMaster.defaultUnitOfMeasurement);
+
+      // Remove properties with empty or default values
+      const filteredRecord = Object.fromEntries(
+        Object.entries(newRecord).filter(([_, value]) => {
+          return value !== '' && value !== 0 && value !== undefined && value !== null;
+        })
+      );
+      console.log(filteredRecord);
+      this.apiService.post<ServiceMaster>('servicenumbers', filteredRecord).subscribe({
+        next: (res: ServiceMaster) => {
+          console.log('service master created:', res);
+
+        }, error: (err) => {
+          console.log(err);
+
+        }, complete: () => {
+          this.confirmationService.confirm({
+            message: `All Records Added successfully. Click Yes to go to the Main Page.`,
+            header: 'Added Successfully',
+            icon: 'pi pi-check',
+            accept: () => {
+              this.router.navigate(['/servicemaster']);
+            },
+            reject: () => {
+            }
+          });
+        }
+      })
+   
+  }
+  cancelFromExcel(item: any): void {
+    this.parsedData = this.parsedData.filter(i => i !== item);
+  }
+
 
   serviceMasterForm = new FormGroup({
     serviceNumberCode: new FormControl(0),
@@ -71,7 +201,7 @@ export class AddEditServiceMasterComponent implements OnInit {
         this.editMode = true;
         this.pageId = state?.serviceNumberCode;
         console.log(this.serviceMasterInfo);
-        
+
       }
     }
   }
@@ -93,34 +223,34 @@ export class AddEditServiceMasterComponent implements OnInit {
   }
 
   getServiceMasterById(id: number) {
-      // this.apiService.getID<ServiceMaster>('servicenumbers',id).subscribe({
-      // next: (res: ServiceMaster) => {
-      //   console.log(res);
-      //   this.serviceMasterInfo = res;
-      // }, error: (err: any) => {
-      //   console.log(err);
+    // this.apiService.getID<ServiceMaster>('servicenumbers',id).subscribe({
+    // next: (res: ServiceMaster) => {
+    //   console.log(res);
+    //   this.serviceMasterInfo = res;
+    // }, error: (err: any) => {
+    //   console.log(err);
 
-      // }, complete: () => {
-      if(this.serviceMasterInfo){
-        this.serviceMasterForm.patchValue({
-          serviceNumberCode:this.serviceMasterInfo?.serviceNumberCode,
-          searchTerm:this.serviceMasterInfo?.searchTerm,
-          description:this.serviceMasterInfo?.description,
-          serviceText:this.serviceMasterInfo?.serviceText,
-          shortTextChangeAllowed:this.serviceMasterInfo?.shortTextChangeAllowed,
-          deletionIndicator:this.serviceMasterInfo?.deletionIndicator,
-          serviceTypeCode:this.serviceMasterInfo?.serviceTypeCode,
-          baseUnitOfMeasurement:this.serviceMasterInfo?.baseUnitOfMeasurement  ,
-          numberToBeConverted:this.serviceMasterInfo?.numberToBeConverted,
-          toBeConvertedUnitOfMeasurement:this.serviceMasterInfo?.toBeConvertedUnitOfMeasurement,
-          convertedNumber:this.serviceMasterInfo?.convertedNumber,
-          defaultUnitOfMeasurement:this.serviceMasterInfo?.defaultUnitOfMeasurement,
-          mainItem:this.serviceMasterInfo?.mainItem,
-          materialGroupCode:this.serviceMasterInfo?.materialGroupCode
+    // }, complete: () => {
+    if (this.serviceMasterInfo) {
+      this.serviceMasterForm.patchValue({
+        serviceNumberCode: this.serviceMasterInfo?.serviceNumberCode,
+        searchTerm: this.serviceMasterInfo?.searchTerm,
+        description: this.serviceMasterInfo?.description,
+        serviceText: this.serviceMasterInfo?.serviceText,
+        shortTextChangeAllowed: this.serviceMasterInfo?.shortTextChangeAllowed,
+        deletionIndicator: this.serviceMasterInfo?.deletionIndicator,
+        serviceTypeCode: this.serviceMasterInfo?.serviceTypeCode,
+        baseUnitOfMeasurement: this.serviceMasterInfo?.baseUnitOfMeasurement,
+        numberToBeConverted: this.serviceMasterInfo?.numberToBeConverted,
+        toBeConvertedUnitOfMeasurement: this.serviceMasterInfo?.toBeConvertedUnitOfMeasurement,
+        convertedNumber: this.serviceMasterInfo?.convertedNumber,
+        defaultUnitOfMeasurement: this.serviceMasterInfo?.defaultUnitOfMeasurement,
+        mainItem: this.serviceMasterInfo?.mainItem,
+        materialGroupCode: this.serviceMasterInfo?.materialGroupCode
 
-        })
-      }// end if
-     // }
+      })
+    }// end if
+    // }
     //})
   }
 
@@ -128,18 +258,18 @@ export class AddEditServiceMasterComponent implements OnInit {
     const value = form.value;
     console.log(value);
     if (this.editMode) {
-        this.serviceMasterService.updateRecord(this.pageId, form.value);
-        this.serviceMasterService.getRecords();
-        this.confirmationService.confirm({
-          message: `ServiceMaster ${this.pageId} Updated successfully. Click Yes to go to the Main Page.`,
-          header: 'Updated Successfully',
-          icon: 'pi pi-check',
-          accept: () => {
-            this.router.navigate(['/servicemaster']);
-          },
-          reject: undefined
-        });
-      
+      this.serviceMasterService.updateRecord(this.pageId, form.value);
+      this.serviceMasterService.getRecords();
+      this.confirmationService.confirm({
+        message: `ServiceMaster ${this.pageId} Updated successfully. Click Yes to go to the Main Page.`,
+        header: 'Updated Successfully',
+        icon: 'pi pi-check',
+        accept: () => {
+          this.router.navigate(['/servicemaster']);
+        },
+        reject: undefined
+      });
+
     } else {
       const newRecord = new ServiceMaster(value.searchTerm, value.description,
         value.serviceText, value.shortTextChangeAllowed,
@@ -148,33 +278,33 @@ export class AddEditServiceMasterComponent implements OnInit {
         value.serviceTypeCode, value.materialGroupCode,
         value.baseUnitOfMeasurement, value.toBeConvertedUnitOfMeasurement, value.defaultUnitOfMeasurement);
 
-        // Remove properties with empty or default values
-        const filteredRecord = Object.fromEntries(
-          Object.entries(newRecord).filter(([_, value]) => {
-            return value !== '' && value !== 0 && value !== undefined && value !== null;
-          })
-        );
-        console.log(filteredRecord);
-        this.apiService.post<ServiceMaster>('servicenumbers', filteredRecord).subscribe({
-    next:(res:ServiceMaster)=>{
-      console.log('service master created:', res);
-     
-    },error:(err)=>{
-      console.log(err);
-      
-    },complete:()=>{
-      this.confirmationService.confirm({
-        message: `ServiceMaster  Added successfully. Click Yes to go to the Main Page.`,
-        header: 'Added Successfully',
-        icon: 'pi pi-check',
-        accept: () => {
-          this.router.navigate(['/servicemaster']);
-        },
-        reject: () => {
-        }
-      });
-    }
+      // Remove properties with empty or default values
+      const filteredRecord = Object.fromEntries(
+        Object.entries(newRecord).filter(([_, value]) => {
+          return value !== '' && value !== 0 && value !== undefined && value !== null;
         })
+      );
+      console.log(filteredRecord);
+      this.apiService.post<ServiceMaster>('servicenumbers', filteredRecord).subscribe({
+        next: (res: ServiceMaster) => {
+          console.log('service master created:', res);
+
+        }, error: (err) => {
+          console.log(err);
+
+        }, complete: () => {
+          this.confirmationService.confirm({
+            message: `ServiceMaster  Added successfully. Click Yes to go to the Main Page.`,
+            header: 'Added Successfully',
+            icon: 'pi pi-check',
+            accept: () => {
+              this.router.navigate(['/servicemaster']);
+            },
+            reject: () => {
+            }
+          });
+        }
+      })
     }
   }
 }
--- src/app/new-service-master/new-service-master.component.ts
+++ src/app/new-service-master/new-service-master.component.ts
@@ -1,144 +1,197 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { NavigationExtras, Router } from '@angular/router';
-import * as FileSaver from 'file-saver';
+
 import { ServiceMaster } from './new-service-master.model';
 import { ServiceMasterService } from './new-service-master.service';
 import { Subscription } from 'rxjs';
 import { ApiService } from '../shared/ApiService.service';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
 
 interface Column {
-  field: string;
-  header: string;
+    field: string;
+    header: string;
 }
 
 
 @Component({
-  selector: 'app-new-service-master',
-  templateUrl: './new-service-master.component.html',
-  styleUrls: ['./new-service-master.component.css'],
-  
-  providers: [MessageService, ConfirmationService, ServiceMasterService]
+    selector: 'app-new-service-master',
+    templateUrl: './new-service-master.component.html',
+    styleUrls: ['./new-service-master.component.css'],
+
+    providers: [MessageService, ConfirmationService, ServiceMasterService]
 })
 export class NewServiceMasterComponent {
 
-  
-      subscription!: Subscription;
-      serviceMasterRecords!: ServiceMaster[];
-      filteredRecords: ServiceMaster[] = this.serviceMasterRecords;
-      // to change Columns View 
-      cols!: Column[];
-      selectedColumns!: Column[];
-      // to handel checkbox selection:
-      selectedRecord: ServiceMaster | null = null;
-      selectedRecords: any[] = [];
-      editMode = false;
-  
-      constructor(private apiService: ApiService, private serviceMasterService: ServiceMasterService,
-          private router: Router, private cd: ChangeDetectorRef) { }
-  
-      ngOnInit() {
-          this.serviceMasterService.getRecords();
-          this.subscription = this.serviceMasterService.recordsChanged.subscribe((records: ServiceMaster[]) => {
-              this.serviceMasterRecords = records;
-              this.filteredRecords = records.sort((a, b) => b.serviceNumberCode - a.serviceNumberCode);
-          });
-          this.cd.markForCheck();
-          this.cols = [
-              { field: 'serviceNumberCode', header: 'Service Master Code' },
-              { field: 'searchTerm', header: 'Search Term' },
-              { field: 'description', header: 'Description' },
-              { field: 'lastChangeDate', header: 'Last Change Date' },
-              { field: 'serviceTypeCode', header: 'Service Type' }
-          ];
-          this.selectedColumns = this.cols;
-      }
-  
-      onRecordSelectionChange(event: any, record: any) {
-          if (event.checked) {
-              this.selectedRecord = record
-              // Add the record to the selectedRecords array if it's not already present
-              if (!this.selectedRecords.includes(record)) {
-                  this.selectedRecords.push(record);
-              }
-          } else {
-              // Remove the record from the selectedRecords array
-              const index = this.selectedRecords.indexOf(record);
-              if (index !== -1) {
-                  this.selectedRecords.splice(index, 1);
-              }
-          }
-      }
-      // To handle Search Input 
-      searchValue: string = '';
-      onSearchInputChange(): void {
-          const keyword = this.searchValue
-          if (keyword !== '') {
-              this.apiService.get<ServiceMaster[]>('servicenumbers/search', keyword).subscribe(response => {
-                  console.log(response);
-                  this.filteredRecords = response
-              });
-          }
-          else {
-              this.filteredRecords = this.serviceMasterRecords;
-          }
-      }
-  
-      onColumnSelectionChange() {
-          this.selectedColumns = this.cols.filter(col => this.selectedColumns.includes(col));
-      }
-  
-      navigateEditService() {
-          const navigationExtras: NavigationExtras = {
-              state: {
-                  Record: this.selectedRecord,
-              }
-          };
-          if (this.selectedRecords.length > 0) {
-              this.router.navigate(['/add-edit-servicemaster'], navigationExtras);
-          }
-      }
-      navigateCopyService() {
-          const navigationExtras: NavigationExtras = {
-              state: {
-                  Record: this.selectedRecord,
-                  Copy: true
-              }
-          };
-          if (this.selectedRecords.length > 0) {
-              this.router.navigate(['/add-edit-servicemaster'], navigationExtras);
-          }
-      }
-      navigateAddServices() {
-          this.router.navigate(['/add-edit-servicemaster']);
-      }
 
-      navigateDetailServices() {
-          const navigationExtras: NavigationExtras = {
-              state: {
-                  RecordDetails: this.selectedRecord,
-              }
-          };
-          if (this.selectedRecords.length > 0) {
-              this.router.navigate(['/detail-servicemaster'], navigationExtras);
-          }
-      }
-      // Export Service Master Data to Excel Sheet
-      exportExcel() {
-          import('xlsx').then((xlsx) => {
-              const selectedRows = this.selectedRecords.length > 0 ? this.selectedRecords : this.serviceMasterRecords;
-              const worksheet = xlsx.utils.json_to_sheet(selectedRows);
-              const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-              const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-              this.saveAsExcelFile(excelBuffer, 'services');
-          });
-      }
-      saveAsExcelFile(buffer: any, fileName: string): void {
-          let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-          let EXCEL_EXTENSION = '.xlsx';
-          const data: Blob = new Blob([buffer], {
-              type: EXCEL_TYPE
-          });
-          FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-      }
+    displayImportsDialog = false;
+    displayExcelDialog = false;
+    // Excel Import:
+    parsedData: ServiceMaster[] = []; // Parsed data from the Excel file
+    displayedColumns: string[] = []; // Column headers from the Excel file
+
+    // imports:
+    showImportsDialog() {
+        this.displayImportsDialog = true;
+    }
+    showExcelDialog() {
+        this.displayExcelDialog = true;
+    }
+
+    onFileSelect(event: any, fileUploader: any) {
+        console.log('Records before :', this.parsedData);
+        const file = event.files[0];
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+            const binaryData = e.target.result;
+            const workbook = XLSX.read(binaryData, { type: 'binary' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const jsonData: any[][] = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+            if (jsonData.length > 0) {
+                this.displayedColumns = jsonData[0].filter((col: any) => typeof col === 'string' && col.trim() !== '') as string[];
+                this.parsedData = jsonData.slice(1).map((row: any[]) => {
+                    const rowData: any = {};
+                    this.displayedColumns.forEach((col, index) => {
+                        rowData[col] = row[index] !== undefined ? row[index] : '';
+                    });
+                    return rowData;
+                });
+                console.log('Records :', this.parsedData);
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Records copied from the excel sheet successfully!',
+                    life: 4000
+                });
+            } else {
+                this.displayedColumns = [];
+                this.parsedData = [];
+            }
+            // Reset the file input using the PrimeNG method
+            fileUploader.clear();
+        };
+        reader.readAsBinaryString(file);
+    }
+
+
+    subscription!: Subscription;
+    serviceMasterRecords!: ServiceMaster[];
+    filteredRecords: ServiceMaster[] = this.serviceMasterRecords;
+    // to change Columns View 
+    cols!: Column[];
+    selectedColumns!: Column[];
+    // to handel checkbox selection:
+    selectedRecord: ServiceMaster | null = null;
+    selectedRecords: any[] = [];
+    editMode = false;
+
+    constructor(private apiService: ApiService, private serviceMasterService: ServiceMasterService,
+        private router: Router, private cd: ChangeDetectorRef, private messageService: MessageService,) { }
+
+    ngOnInit() {
+        this.serviceMasterService.getRecords();
+        this.subscription = this.serviceMasterService.recordsChanged.subscribe((records: ServiceMaster[]) => {
+            this.serviceMasterRecords = records;
+            this.filteredRecords = records.sort((a, b) => b.serviceNumberCode - a.serviceNumberCode);
+        });
+        this.cd.markForCheck();
+        this.cols = [
+            { field: 'serviceNumberCode', header: 'Service Master Code' },
+            { field: 'searchTerm', header: 'Search Term' },
+            { field: 'description', header: 'Description' },
+            { field: 'lastChangeDate', header: 'Last Change Date' },
+            { field: 'serviceTypeCode', header: 'Service Type' }
+        ];
+        this.selectedColumns = this.cols;
+    }
+
+    onRecordSelectionChange(event: any, record: any) {
+        if (event.checked) {
+            this.selectedRecord = record
+            // Add the record to the selectedRecords array if it's not already present
+            if (!this.selectedRecords.includes(record)) {
+                this.selectedRecords.push(record);
+            }
+        } else {
+            // Remove the record from the selectedRecords array
+            const index = this.selectedRecords.indexOf(record);
+            if (index !== -1) {
+                this.selectedRecords.splice(index, 1);
+            }
+        }
+    }
+    // To handle Search Input 
+    searchValue: string = '';
+    onSearchInputChange(): void {
+        const keyword = this.searchValue
+        if (keyword !== '') {
+            this.apiService.get<ServiceMaster[]>('servicenumbers/search', keyword).subscribe(response => {
+                console.log(response);
+                this.filteredRecords = response
+            });
+        }
+        else {
+            this.filteredRecords = this.serviceMasterRecords;
+        }
+    }
+
+    onColumnSelectionChange() {
+        this.selectedColumns = this.cols.filter(col => this.selectedColumns.includes(col));
+    }
+
+    navigateEditService() {
+        const navigationExtras: NavigationExtras = {
+            state: {
+                Record: this.selectedRecord,
+            }
+        };
+        if (this.selectedRecords.length > 0) {
+            this.router.navigate(['/add-edit-servicemaster'], navigationExtras);
+        }
+    }
+    navigateCopyService() {
+        const navigationExtras: NavigationExtras = {
+            state: {
+                Record: this.selectedRecord,
+                Copy: true
+            }
+        };
+        if (this.selectedRecords.length > 0) {
+            this.router.navigate(['/add-edit-servicemaster'], navigationExtras);
+        }
+    }
+    navigateAddServices() {
+        this.router.navigate(['/add-edit-servicemaster']);
+    }
+
+    navigateDetailServices() {
+        const navigationExtras: NavigationExtras = {
+            state: {
+                RecordDetails: this.selectedRecord,
+            }
+        };
+        if (this.selectedRecords.length > 0) {
+            this.router.navigate(['/detail-servicemaster'], navigationExtras);
+        }
+    }
+    // Export Service Master Data to Excel Sheet
+    exportExcel() {
+        import('xlsx').then((xlsx) => {
+            const selectedRows = this.selectedRecords.length > 0 ? this.selectedRecords : this.serviceMasterRecords;
+            const worksheet = xlsx.utils.json_to_sheet(selectedRows);
+            const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+            const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            this.saveAsExcelFile(excelBuffer, 'services');
+        });
+    }
+    saveAsExcelFile(buffer: any, fileName: string): void {
+        let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE
+        });
+        FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+    }
 }
